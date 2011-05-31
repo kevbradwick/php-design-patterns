f3744b6770e864b9e905a4cde9362bef881cba35
Kevin Bradwick <kbradwick@gmail.com>
2011-05-31 19:22:37 +0100

    added factory pattern

--- Patterns/Factory/Cars/Ferrari.php
+++ Patterns/Factory/Cars/Ferrari.php
@@ -2,20 +2,41 @@
 
 require_once 'CarInterface.php';
 
+/**
+ * Ferrari
+ *
+ * @author Kevin Bradwick <kbradwick@gmail.com>
+ * @link   https://github.com/kevbradwick/php-design-patterns
+ */
 class Ferrari implements CarInterface
 {
+    /**
+     * {@inheritdoc}
+     * 
+     * @return string
+     */
     public function getModel()
     {
-        return 'F355';
+        return 'Berlinetta';
     }
 
+    /**
+     * {@inheritdoc}
+     * 
+     * @return string
+     */
     public function getManufacturer()
     {
         return 'Ferrari';
     }
 
+    /**
+     * {@inheritdoc}
+     * 
+     * @return string
+     */
     public function getMake()
     {
-
+        return 'F355';
     }
 }
